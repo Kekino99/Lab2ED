7acbdf7d31950e1c6d25649f36601a66be51eea7
Sergi Simón <sergisiba@gmail.com>
2018-10-10 11:53:45 +0200

    Implementa tots els testos, però sense fer proves de jerarquía TODO: afegir jerarquía de clases i fer testos amb jerarquía.

---
+++ Tests/TaskTest.java
@@ -0,0 +1,67 @@
+import org.junit.Before;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+public class TaskTest {
+    private List<Long> list1;
+
+    @Before
+    public void init () {
+        list1 = Arrays.asList(1L, 2L, 3L, 4L, 5L);
+    }
+
+    @org.junit.Test
+    public void testWithinRange() {
+        List<Integer> list2 = new ArrayList<Integer>();
+        for(int i = 0; i<10; i++) {
+            list2.add(i);
+        }
+        assertEquals(Arrays.asList( 1, 2, 3, 4), Task.withinRange(list2, 5, 1));
+        assertEquals(Collections.emptyList(), Task.withinRange(list2, 1, 2));
+        assertEquals(Arrays.asList(3, 4, 5, 6), Task.withinRange(list2, 7, 3));
+    }
+
+    @org.junit.Test
+    public void testWithinRange1() {
+        assertEquals(Arrays.asList(3L, 4L, 5L), Task.withinRange(list1, 6L, 3L));
+        assertEquals(Collections.emptyList(), Task.withinRange(list1, 1L, 2L));
+
+    }
+
+    @org.junit.Test
+    public void testWithinTange() {
+        assertEquals(Arrays.asList(3L, 4L, 5L), Task.withinTange((Comparator<Long>) (d1, d2) -> d1.compareTo(d2),
+                list1, 6L, 3L));
+        assertEquals(Collections.emptyList(), Task.withinTange((Comparator<Long>) (d1, d2) -> d1.compareTo(d2),
+                list1, 1L, 2L));
+    }
+
+    @org.junit.Test
+    public void testCopyWithRange() {
+        List<Long> list3 = new ArrayList<Long>();
+        Task.copyWithRange(list1, list3, 1L, 2l);
+        assertEquals(Collections.emptyList(), list3);
+        Task.copyWithRange(list1, list3, 5L, 2L);
+        assertEquals(Arrays.asList(2L, 3L, 4L), list3);
+        list3 = Arrays.asList(10L, 11L, 12L, 13L, 14L);
+        Task.copyWithRange(list1, list3, 5L, 2L);
+        assertEquals(Arrays.asList(2L, 3L, 4L, 13L, 14L), list3);
+    }
+
+    @org.junit.Test
+    public void testCopyWithTange() {
+        List<Long> list3 = new ArrayList<Long>();
+        Task.copyWithTange((Comparator<Long>) (d1, d2) -> d1.compareTo(d2), list1, list3, 1L, 2L);
+        assertEquals(Collections.emptyList(), list3);
+        Task.copyWithTange((Comparator<Long>) (d1, d2) -> d1.compareTo(d2), list1, list3, 5L, 2L);
+        assertEquals(Arrays.asList(2L, 3L, 4L), list3);
+        list3 = Arrays.asList(10L, 11L, 12L, 13L, 14L);
+        Task.copyWithTange((Comparator<Long>) (d1, d2) -> d1.compareTo(d2), list1, list3, 5L, 2L);
+        assertEquals(Arrays.asList(2L, 3L, 4L, 13L, 14L), list3);
+
+    }
+
+
+}
--- src/Task.java
+++ src/Task.java
@@ -40,27 +40,35 @@ public class Task {
         return result;
     }
 
-    public static <E extends Comparable<? super E>> void copyWithRange(List<? extends E> trg, List<? super E> src,
+    public static <E extends Comparable<? super E>> void copyWithRange(List<? extends E> src, List<? super E> trg,
                                                                        E max, E min) {
-        Iterator<? extends E> trgit = trg.iterator();
-        ListIterator<? super E> srcit = src.listIterator();
-        while(trgit.hasNext() && srcit.hasNext()) {
-            E elem = trgit.next();
+        Iterator<? extends E> srcit = src.iterator();
+        ListIterator<? super E> trgit = trg.listIterator();
+        while(srcit.hasNext()) {
+            E elem = srcit.next();
             if (min.compareTo(elem) <= 0 && elem.compareTo(max) < 0) {
-                srcit.next();
-                srcit.set(elem);
+                if (trgit.hasNext()) {
+                    trgit.next();
+                    trgit.set(elem);
+                } else {
+                    trgit.add(elem);
+                }
             }
         }
     }
 
-    public static <E> void copyWithTange(Comparator<E> comp, List<? extends E> trg, List<? super E> src, E min, E max) {
-        Iterator<? extends E> trgit = trg.iterator();
-        ListIterator<? super E> srcit = src.listIterator();
-        while(trgit.hasNext() && srcit.hasNext()) {
-            E elem = trgit.next();
+    public static <E> void copyWithTange(Comparator<E> comp, List<? extends E> src, List<? super E> trg, E max, E min) {
+        Iterator<? extends E> srcit = src.iterator();
+        ListIterator<? super E> trgit = trg.listIterator();
+        while(srcit.hasNext()) {
+            E elem = srcit.next();
             if (comp.compare(min, elem) <= 0 && comp.compare(elem, max) < 0) {
-                srcit.next();
-                srcit.set(elem);
+                if (trgit.hasNext()) {
+                    trgit.next();
+                    trgit.set(elem);
+                } else {
+                    trgit.add(elem);
+                }
             }
         }
     }
