ab509918bda5ebd0d860f85f5061822a6da495aa
Sergi Simón <sergisiba@gmail.com>
2018-10-12 20:11:29 +0200

    S'ha canviat capceleres per generalitzar. Falta fer els testos de jerarquia.

--- src/task/Task.java
+++ src/task/Task.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Task {
 
-//Comentari per provar el Git
+//Comentari per provar el Git. Funciona.
     public static List<Integer> withinRange(List<Integer> l, int max, int min) {
         List<Integer> result = new ArrayList<>();
         Iterator<Integer> it = l.iterator();
@@ -17,9 +17,9 @@ public class Task {
         return result;
     }
 
-    public static <E extends Comparable<? super E>> List<E> withinRange(List<E> l, E max, E min) {
+    public static <E extends Comparable<? super E>> List<E> withinRange(List<? extends E> l, E max, E min) {
         List<E> result = new ArrayList<>();
-        Iterator<E> it = l.iterator();
+        Iterator<? extends E> it = l.iterator();
         while (it.hasNext()) {
             E elem = it.next();
             if (min.compareTo(elem) <= 0 && elem.compareTo(max) < 0) {
@@ -29,9 +29,9 @@ public class Task {
         return result;
     }
 
-    public static <E> List<E> withinTange(Comparator<E> comp, List<E> l, E max, E min) {
+    public static <E> List<E> withinTange(Comparator<? super E> comp, List<? extends E> l, E max, E min) {
         List<E> result = new ArrayList<>();
-        Iterator<E> it = l.iterator();
+        Iterator<? extends E> it = l.iterator();
         while (it.hasNext()) {
             E elem = it.next();
             if (comp.compare(min, elem) <= 0 && comp.compare(elem, max) < 0) {
@@ -58,7 +58,7 @@ public class Task {
         }
     }
 
-    public static <E> void copyWithTange(Comparator<E> comp, List<? extends E> src, List<? super E> trg, E max, E min) {
+    public static <E> void copyWithTange(Comparator<? super E> comp, List<? extends E> src, List<? super E> trg, E max, E min) {
         Iterator<? extends E> srcit = src.iterator();
         ListIterator<? super E> trgit = trg.listIterator();
         while(srcit.hasNext()) {
